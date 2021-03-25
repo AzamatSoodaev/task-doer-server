2b03132b67d4567a1f295c7e07d5124a08ac5ec3
Azamat Soodaev <dreamy.kg33@gmail.com>
2021-03-25 17:08:52 +0600

    conn

--- src/models/index.js
+++ src/models/index.js
@@ -11,7 +11,16 @@ const { Sequelize, DataTypes } = require("sequelize");
 //   config
 // );
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 sequelize
   .authenticate()
