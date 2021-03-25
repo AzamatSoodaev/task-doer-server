9cf706aca63d79ebe613cff4e9f114d92f91cd04
Azamat Soodaev <dreamy.kg33@gmail.com>
2021-03-25 16:15:15 +0600

    removed unused fields

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,21 +1,8 @@
 require("dotenv").config();
 
-const { DB_USER, DB_HOST, DB_NAME, DB_PASS } = process.env;
-
 module.exports = {
-  development: {
-    username: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: "postgres",
-  },
+  development: {},
   production: {
-    username: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-    host: DB_HOST,
-    dialect: "postgres",
     logging: false,
   },
 };
